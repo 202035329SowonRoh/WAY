066ba89343912635bccbc3dc27e58881b14a955a
RohSowon <sowon4590@gachon.ac.kr>
2023-05-24 21:32:43 +0900

    ADD Main & Challenge

---
+++ app/src/main/java/com/example/way/ChallengeActivity.java
@@ -0,0 +1,17 @@
+package com.example.way;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class ChallengeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge);
+
+    }
+}
--- app/src/main/java/com/example/way/MainActivity.java
+++ app/src/main/java/com/example/way/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.way;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.opengl.GLES30;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btn_challenge = findViewById(R.id.btn_challenge);
+
+        btn_challenge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chlngIntent = new Intent(MainActivity.this, ChallengeActivity.class);
+                startActivity(chlngIntent);
+                finish();
+            }
+        });
     }
 }
